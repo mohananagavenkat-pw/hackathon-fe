13a6a90400d07314b7356167559bb0feb3821325
Savyashree A <savyashreea@gmail.com>
2023-10-09 17:30:00 +0530

    created components folder

--- src/router/router.js
+++ src/router/router.js
@@ -4,29 +4,38 @@ import { createBrowserRouter } from "react-router-dom";
 import User from "../pages/user";
 import Admin from "../pages/admin";
 import CheckProtectedRoute from "../hoc/CheckProtectedRoute";
+import Homepage from "../components/Homepage";
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <User />,
-	},
-	{
-		path: "/admin",
-		element: (
-			<CheckProtectedRoute role="admin">
-				<Admin />
-			</CheckProtectedRoute>
-		),
+  {
+    path: "/",
+    element: <User />,
+  },
+  {
+    path: "/admin",
+    element: (
+      <CheckProtectedRoute role="admin">
+        <Admin />
+      </CheckProtectedRoute>
+    ),
 
-		children: [{}],
-	},
-	{
-		path: "/user",
-		element: (
-			<CheckProtectedRoute role="admin">
-				<User />
-			</CheckProtectedRoute>
-		),
-	},
+    children: [{}],
+  },
+  {
+    path: "/user",
+    element: (
+      <CheckProtectedRoute role="admin">
+        <User />
+      </CheckProtectedRoute>
+    ),
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <CheckProtectedRoute role="user">
+        <Homepage />
+      </CheckProtectedRoute>
+    ),
+  },
 ]);
 
 export default router;
