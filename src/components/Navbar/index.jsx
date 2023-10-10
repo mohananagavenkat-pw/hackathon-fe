4b5efc1d9461b0f493f228a484ae98c9fd3a146d
Rudresh <ineuron@Ineurons-MacBook-Pro.local>
2023-10-10 19:18:46 +0530

    changes in Navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,9 +5,8 @@ import PWskills from "../../assets/PWskills";
 
 function Navbar({color}) {
   return (
-    <nav className="bg-black ">
+    <nav className={`bg-${color === "white" ? 'black' : "white"} `}>
       <Container className="p-4 medium:w-[85%] medium:m-auto large:w-[95%] flex justify-between">
-        {/* <img src={pwSkillsLogo} alt="PwSkillsLogo" /> */}
         <PWskills color = {color}/>
         <Button>Registration</Button>
       </Container>
