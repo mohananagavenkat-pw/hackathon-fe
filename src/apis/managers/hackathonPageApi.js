2c96dd3b46383ff549f3e01ccf634286666d71a0
bitlaniineuron12 <mukesh@ineuron.ai>
2023-10-13 16:59:34 +0530

    Update hackathonPageApi.js

--- src/apis/managers/hackathonPageApi.js
+++ src/apis/managers/hackathonPageApi.js
@@ -6,6 +6,11 @@ class HackathonApi {
   static async getRounds(hackathonId) {
     return get(`/user/fetch-by-rounds/?hackathonId=${hackathonId}`);
   }
+
+  static async submitHackathon(data) {
+
+    return post(`/user-hackathon/submit`, data)
+  }
 }
 
 export default HackathonApi;
